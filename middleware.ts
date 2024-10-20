7d3b86bb09f9d75f2e360d2d8a745aea98a5e4d5
radovanbotik <radovanbotik@gmail.com>
2024-10-20 22:14:22 +0200

    initialized

---
+++ middleware.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+
+export async function middleware(request: NextRequest){
+    console.log(request)
+    return NextResponse.next()
+}
