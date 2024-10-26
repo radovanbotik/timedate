b901a005c091c312f390e0257ee4fac584b55b38
radovanbotik <radovanbotik@gmail.com>
2024-10-26 14:22:19 +0200

    useeffect

--- app/components/DisplayTime.tsx
+++ app/components/DisplayTime.tsx
@@ -10,19 +10,26 @@ type TTime = {
 
 export function DisplayTime({ serverTime, locale }: TTime) {
   const [time, setTime] = useState(new Date());
+  const [isInit, setIsInit] = useState(false);
   useEffect(() => {
+    if (window !== undefined) setIsInit(true);
     const interval = setInterval(() => {
       const newTime = generateTime();
       setTime(newTime);
     }, 1000);
     return function cleanUp() {
       clearInterval(interval);
+      setIsInit(false);
     };
   }, []);
   const localizedTime = toLocalTime(time, locale);
   return (
-    <time className="tabular-nums text-7xl" suppressHydrationWarning>
-      {localizedTime}
-    </time>
+    <>
+      {isInit && (
+        <time className="tabular-nums text-7xl" suppressHydrationWarning>
+          {localizedTime}
+        </time>
+      )}
+    </>
   );
 }
